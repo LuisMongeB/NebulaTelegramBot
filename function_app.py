57f0cf7aff847afa4611b0c348a558a57c1d6831
Luis <luigimonge16@gmail.com>
2025-02-01 21:31:01 +0100

    MODIFY: make TelgramBotFunction async

--- function_app.py
+++ function_app.py
@@ -1,4 +1,3 @@
-import asyncio
 import logging
 import os
 
@@ -24,9 +23,8 @@ command_registry.register(
     "start",
     start_command_handler.execute_with_name,
     "Start the bot",
-    "Initialize the bot and see welcome message",
+    "Initialize the bot and see the welcome message",
 )
-
 
 app = func.FunctionApp()
 
@@ -35,10 +33,9 @@ app.register_blueprint(bp)
 
 @app.function_name("TelegramBotFunction")
 @app.route(route="nebula", methods=["POST"], auth_level=func.AuthLevel.ANONYMOUS)
-def telegram_bot_function(req: func.HttpRequest) -> func.HttpResponse:
+async def telegram_bot_function(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Python HTTP trigger function received a request from Telegram Bot.")
 
-    logging.info(f"Instantiated the TelegramService class.")
     try:
         req_body = req.get_json()
         logging.info(f"Received message: {req_body}")
@@ -50,14 +47,8 @@ def telegram_bot_function(req: func.HttpRequest) -> func.HttpResponse:
             logging.info("Ignoring non-relevant message.")
             return func.HttpResponse(status_code=200)
 
-        # Check the type of message
         message = req_body.get("message", {})
 
-        if "message_auto_delete_timer_changed" in req_body.get("message", {}):
-            logging.info("Received an auto-delete timer change message.")
-            return func.HttpResponse(status_code=200)
-
-        # handle commands
         if "entities" in message and message["entities"][0]["type"] == "bot_command":
 
             command_name = message["text"]
@@ -65,9 +56,10 @@ def telegram_bot_function(req: func.HttpRequest) -> func.HttpResponse:
             username = message["from"].get("username", "")
 
             if command_name == "/start":
-                response = asyncio.run(
-                    start_command_handler.execute_with_name(chat_id, username=username)
+                response = await start_command_handler.execute_with_name(
+                    chat_id, username=username
                 )
+
                 if response:
                     return func.HttpResponse(
                         "Start command executed correctly.", status_code=200
@@ -81,56 +73,48 @@ def telegram_bot_function(req: func.HttpRequest) -> func.HttpResponse:
         if "text" in message:
             text = message["text"]
             logging.info(f"Received text message: {text}")
-            logging.info(f"Message: ", message)
-            asyncio.run(
-                telegram_service.send_message(
-                    chat_id=message["chat"]["id"], text=f"Received text message: {text}"
-                )
+            await telegram_service.send_message(
+                chat_id=message["chat"]["id"], text=f"Received text message: {text}"
             )
             return func.HttpResponse(f"Received text message: {text}", status_code=200)
 
         elif "audio" in message or "voice" in message:
-            # SET DURATION LIMIT -> TODO: good for now but do it better, don't hardcode it
+            # SET DURATION LIMIT -> TODO: good for now but improve later, don't hardcode it
             duration = message.get("audio", message.get("voice", {})).get("duration", 0)
-            if duration < 600:
-                asyncio.run(
-                    telegram_service.send_message(
-                        chat_id=message["chat"]["id"],
-                        text="Processing audio message...",
-                    )
+            if duration < 420:
+                await telegram_service.send_message(
+                    chat_id=message["chat"]["id"],
+                    text="Processing audio message...",
                 )
-                transcription, language = asyncio.run(
-                    audio_processor.process_audio_message(message, token)
+
+                transcription, language = await audio_processor.process_audio_message(
+                    message, token
                 )
 
                 if duration > 90:
-                    final_transcription = asyncio.run(
-                        summarize_transcription(transcription, language, openai_service)
+                    final_transcription = await summarize_transcription(
+                        transcription, language, openai_service
                     )
-                    asyncio.run(
-                        telegram_service.send_message(
-                            chat_id=message["chat"]["id"], text=f"{final_transcription}"
-                        )
+
+                    await telegram_service.send_message(
+                        chat_id=message["chat"]["id"], text=f"{final_transcription}"
+                    )
+                    return func.HttpResponse(
+                        "Transcription has been summarized", status_code=200
                     )
                 else:
                     final_transcription = transcription
-                    asyncio.run(
-                        telegram_service.send_message(
-                            chat_id=message["chat"]["id"],
-                            text=f"[{language}] Transcription:\n{final_transcription}",
-                        )
-                    )
-
-                return func.HttpResponse(
-                    "Audio messages longer than 10 minute are not supported.",
-                    status_code=200,
-                )
-            else:
-                asyncio.run(
-                    telegram_service.send_message(
+                    await telegram_service.send_message(
                         chat_id=message["chat"]["id"],
-                        text="Audio messages longer than 10 minute are not supported.",
+                        text=f"[{language}] Transcription:\n{final_transcription}",
                     )
+                    return func.HttpResponse(
+                        "Transcription has been sent", status_code=200
+                    )
+            else:
+                await telegram_service.send_message(
+                    chat_id=message["chat"]["id"],
+                    text="Audio messages longer than 10 minute are not supported.",
                 )
                 return func.HttpResponse(
                     "Audio messages longer than 10 minute are not supported.",
@@ -147,44 +131,3 @@ def telegram_bot_function(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(
             "An error occurred while processing the request.", status_code=500
         )
-
-
-# @app.function_name(name="ProcessM4AFile")
-# @app.blob_trigger(
-#     arg_name="myblob", path="filecontainer/{name}.m4a", connection="AdditionalStorage"
-# )
-# @app.blob_output(
-#     arg_name="outputblob",
-#     path="processedcontainer/{name}_processed.m4a",  # Use a placeholder for dynamic blob name
-#     connection="AdditionalStorage",
-# )
-# def process_m4a_blob(myblob: func.InputStream, outputblob: func.Out[func.InputStream]):
-
-#     try:
-#         # Extract the chat_id from the blob name
-#         chat_id = myblob.name.split("/")[-1].split("_")[0]
-#         # Log the blob details
-#         logging.info(
-#             f"Python blob Function triggered after the .m4a file was uploaded to filecontainer.\n"
-#             f"Blob path: {myblob.name}, Blob type: {type(myblob)}"
-#         )
-
-#         m4a_blob_bytes = myblob.read()
-#         transcription, language = asyncio.run(
-#             openai_service.transcribe_audio(audio_data=m4a_blob_bytes)
-#         )
-
-#         # Set the processed content to the output blob
-#         outputblob.set(m4a_blob_bytes)
-
-#         logging.info(
-#             f"Blob has been copied to the processed container with dynamic name."
-#         )
-#         asyncio.run(
-#             telegram_service.send_message(
-#                 chat_id=chat_id, text=f"Transcription:\n{transcription}"
-#             )
-#         )
-#     except Exception as e:
-#         logging.error(f"An error occurred while writing the processed file: {e}")
-#         raise Exception(f"An error occurred while writing the processed file: {e}")
